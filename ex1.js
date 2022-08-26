68d3a3c815c5e369428c924dce2f7a2bade90eba
kalebpenuel <111325121+kalebpenuel@users.noreply.github.com>
2022-08-25 20:36:26 -0500

    Initial commit

---
+++ ex1.js
@@ -0,0 +1,20 @@
+const colorChangeBtn = document.querySelector('.btn-randomizer');
+const text = document.querySelector('h1');
+const bodyEl = document.body;
+// could rename the const to something that works better
+function changeBGColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`;
+    // console.log("hello");
+}
+
+function changeTextColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.color = `#${randomColor}`;
+    // console.log("white text");
+}
+
+
+
+colorChangeBtn.addEventListener('click', changeBGColor);
+colorChangeBtn.addEventListener('click', changeTextColor);
---
+++ ex2.js
@@ -0,0 +1,11 @@
+const countTrue = [
+    true,
+    true,
+    false,
+    true,
+]
+
+const count1 = countTrue.filter(value => value === true).length;
+console.log(count1);
+
+console.log(countTrue);
